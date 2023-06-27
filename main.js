a2a69027b4a9184b8df1378caba7f7cc240881b2
ChiefLopes <chieflopesekede@gmail.com>
2023-06-27 13:23:04 +0100

    added javascript functionalities

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log(window.innerWidth);
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box, index) => {
+  if (index % 2 === 1) {
+    box.style.backgroundColor = "red";
+  }
+});
+
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+  triggerBotttom = window.innerHeight / 5 * 4;
+
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingclientRect().top;
+    if (boxTop < triggerBotttom) {
+        box.classList.add("show")
+    } else {
+    box.classList.remove("show")}
+  }) 
+}
+
+
